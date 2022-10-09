8636d017d411647b81c6688fcd8584dffa40e827
D-Tretyakov <tretyakov.daniil.education@gmail.com>
2022-10-09 22:58:51 +0300

    finished declaration and defs, deleted .cpp file

--- 1_lru_cache/lru_cache.h
+++ 1_lru_cache/lru_cache.h
@@ -1,15 +1,65 @@
+#include <iostream>
 #include <unordered_map>
 #include <list>
 
+// Class declaration
 template<typename T>
 struct LRUCache
 {
-    LRUCache();
-    bool update();
+    LRUCache(int a_size) : m_size(a_size) {};
+    
+    void update(const T& a_entry);
+    const std::list<T>& get_state() const;
+    void debug_print() const;
 
     private:
         using ListIt = typename std::list<T>::iterator;
 
         std::unordered_map<T, ListIt> m_hash;
         std::list<T> m_list;
+
+        int m_size;
+        int m_cache_hits;
 };
+
+
+//Methods definitions
+template<typename T>
+void LRUCache<T>::update(const T& entry)
+{
+    auto found = m_hash.find(entry);
+    if (found != m_hash.end()) 
+    {
+        // iterator to found entry list element
+        auto it = found->second;
+        // move entry to front if it was found 
+        if (found->second != m_list.begin())
+            m_list.splice(m_list.begin(), m_list, it, std::move(it));
+        
+        m_cache_hits += 1;
+    }
+
+    if (m_list.size() == m_size)
+    {
+        m_hash.erase(m_list.back());
+        m_list.pop_back();
+    }
+
+    m_list.push_front(entry);
+    m_hash[entry] = m_list.begin();
+}
+
+template<typename T>
+const std::list<T>& LRUCache<T>::get_state() const
+{
+    return m_list;
+}
+
+template<typename T>
+void LRUCache<T>::debug_print() const
+{
+    std::cout << "Current cached values:\n\t{ ";
+    for (const auto& element : m_list)
+        std::cout << element << " ";
+    std::cout << "}" << std::endl;
+}
