e907b2cf01fc3119a96bceea7be33825f43d6a8e
D-Tretyakov <tretyakov.daniil.education@gmail.com>
2022-10-10 03:41:19 +0300

    some more tests

--- 1_lru_cache/test.cpp
+++ 1_lru_cache/test.cpp
@@ -1,5 +1,9 @@
+#include <random>
+#include <iostream>
+
 #include "gmock/gmock.h"
 #include <gtest/gtest.h>
+
 #include "lru_cache.h"
 
 TEST(LRUCacheTest, InitZeroSize) 
@@ -7,7 +11,7 @@ TEST(LRUCacheTest, InitZeroSize)
     EXPECT_THROW(LRUCache<int> cache{0}, std::runtime_error);
 }
 
-TEST(LRUCacheTest, SimpleOne)
+TEST(LRUCacheTest, ValuesMoreThanSize)
 {
     LRUCache<int> cache{10};
 
@@ -18,5 +22,35 @@ TEST(LRUCacheTest, SimpleOne)
     ASSERT_THAT(
         cache.get_state(), 
         ElementsAre(11, 10, 9, 8, 7, 6, 5, 4, 3, 2)
+    );
+}
+
+TEST(LRUCacheTest, ValuesLessThanSize)
+{
+    LRUCache<int> cache{10};
+
+    for (int i = 0; i < 4; i++)
+        cache.update(i);
+    
+    using namespace testing;
+    ASSERT_THAT(
+        cache.get_state(), 
+        ElementsAre(3, 2, 1, 0)
+    );
+}
+
+TEST(LRUCacheTest, MillionValues)
+{
+    LRUCache<int> cache{10};
+    std::minstd_rand gen(0);
+    std::uniform_int_distribution<> distrib(1, 10);
+
+    for (int i = 0; i < 1000000; i++)
+        cache.update(distrib(gen));
+    
+    using namespace testing;
+    ASSERT_THAT(
+        cache.get_state(), 
+        ElementsAre(1, 1, 7, 9, 10, 2, 6, 4, 3, 8)
     );
 }
