26b9647862ae8c28f36420be17008e23816bcd8c
D-Tretyakov <tretyakov.daniil.education@gmail.com>
2022-10-10 03:13:13 +0300

    added gtests

--- 1_lru_cache/test.cpp
+++ 1_lru_cache/test.cpp
@@ -1,10 +1,29 @@
+#include "gmock/gmock.h"
 #include <gtest/gtest.h>
 #include "lru_cache.h"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(LRUCacheTest, InitZeroSize) 
+{
+    LRUCache<int> cache{0};
+
+    EXPECT_TRUE(cache.get_state().empty());
+    
+    cache.update(11);
+    cache.update(12);
+    cache.update(13);
+    EXPECT_TRUE(cache.get_state().empty());
+}
+
+TEST(LRUCacheTest, SimpleOne)
+{
+    LRUCache<int> cache{10};
+
+    for (int i = 0; i <= 11; i++)
+        cache.update(i);
+    
+    using namespace testing;
+    ASSERT_THAT(
+        cache.get_state(), 
+        ElementsAre(11, 10, 9, 8, 7, 6, 5, 4, 3, 2)
+    );
 }
